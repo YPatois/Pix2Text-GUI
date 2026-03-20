7dd2cb385842d7c2e60d6790ea4ee872c0050adf
Yannick Patois <yannick.patois@iphc.cnrs.fr>
2026-03-20 13:20:55 +0100

    Using UI builder (somewhat)

--- src/p2tgui.py
+++ src/p2tgui.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python
-import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QFile
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("PySide6")
-        self.setGeometry(100, 100, 400, 300)  # (x, y, largeur, hauteur)
+def main():
+    app = QApplication([])
+    loader = QUiLoader()
+    file = QFile("p2tui.ui")
+    file.open(QFile.ReadOnly)
+    window = loader.load(file)
+    window.show()
+    app.exec()
 
-        # Ajouter un bouton
-        self.button = QPushButton("Click me !", self)
-        self.button.move(150, 130)
-        self.button.clicked.connect(self.on_button_click)
-
-    def on_button_click(self):
-        print("Clicked !")
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
+    main()
